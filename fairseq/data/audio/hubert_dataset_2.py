fix valid rand perm; remove logical and between mask_indices and padding_mask; remove sample_size_ctr scaling

fairseq/data/audio/hubert_dataset_2.py
@@ -205,6 +205,11 @@ class HubertDataset_2(FairseqDataset):
         #s = parselmouth.Sound(wav, sampling_frequency=sr)
         _, (lo, hi, _) = self.spk2info[spk]
         
+        if lo==50:
+            lo=75
+        if spk=="1447":
+            lo, hi = 60, 400
+        
         ratio_fs = self.rng.uniform(1, 1.4)
         coin = (self.rng.random() > 0.5)
         ratio_fs = coin*ratio_fs + (1-coin)*(1/ratio_fs)
@@ -217,10 +222,6 @@ class HubertDataset_2(FairseqDataset):
         coin = (self.rng.random() > 0.5)
         ratio_pr = coin*ratio_pr + (1-coin)*(1/ratio_pr)
         
-        if lo==50:
-            lo=75
-        if spk=="1447":
-            lo, hi = 60, 400
         ss = change_gender(wav, sr, lo, hi, ratio_fs, ratio_ps, ratio_pr)
         
         return ss
@@ -230,6 +231,7 @@ class HubertDataset_2(FairseqDataset):
         _, (lo, hi, _) = self.spk2info[spk]
         
         if lo==50:
+            lo=75
             ratio_fs, f0_med, ratio_pr = 1.2, 300, 1.2
         else:
             ratio_fs, f0_med, ratio_pr = 0.8, 100, 0.8
@@ -275,8 +277,10 @@ class HubertDataset_2(FairseqDataset):
             try:
                 wav_2 = self.fixed_formant_f0(wav, cur_sample_rate, spk)
             except UserWarning:
+                wav_2 = np.copy(wav)
                 print(f"Praat warining - {fileName}")
             except RuntimeError:
+                wav_2 = np.copy(wav)
                 print(f"Praat Error - {fileName}")
             wav_2 = torch.from_numpy(wav_2).float()
             wav_2 = self.postprocess(wav_2, cur_sample_rate)

fairseq/models/hubert/hubert_6.py
@@ -126,6 +126,9 @@ class HubertConfig_6(FairseqDataclass):
     logit_temp: float = field(
         default=0.1, metadata={"help": "temperature to divide logits by"}
     )
+    logit_temp_ctr: float = field(
+        default=0.1, metadata={"help": "temperature to divide logits_ctr by"}
+    )
     target_glu: bool = field(
         default=False, metadata={"help": "adds projection + glu to targets"}
     )
@@ -282,6 +285,7 @@ class HubertModel_6(BaseFairseqModel):
 
         self.feature_grad_mult = cfg.feature_grad_mult
         self.logit_temp = cfg.logit_temp
+        self.logit_temp_ctr = cfg.logit_temp_ctr
         self.skip_masked = cfg.skip_masked
         self.skip_nomask = cfg.skip_nomask
         
@@ -337,7 +341,7 @@ class HubertModel_6(BaseFairseqModel):
     def build_model(cls, cfg: HubertConfig_6, task: HubertPretrainingTask_6):
         """Build a new model instance."""
 
-        model = HubertModel_6(cfg, task.cfg, task.dictionaries)
+        model = HubertModel_6(cfg, task.cfg, task.dictionaries)        
         return model
 
     def get_mask(self, B, T, padding_mask):
@@ -428,7 +432,7 @@ class HubertModel_6(BaseFairseqModel):
 
         logits = torch.cosine_similarity(x.float(), targets.float(), dim=-1).type_as(x)
 
-        logits = logits / self.logit_temp
+        logits = logits / self.logit_temp_ctr
 
         if is_xla_tensor(logits) or neg_is_pos.any():
             fillval = -float(2 ** 30)
@@ -538,7 +542,8 @@ class HubertModel_6(BaseFairseqModel):
             mask_indices = mask_indices.repeat(2, 1)
             features[mask_indices] = self.mask_emb
             x = features
-            unmasked_indices = torch.logical_and(~padding_mask, ~mask_indices)
+            #unmasked_indices = torch.logical_and(~padding_mask, ~mask_indices)
+            unmasked_indices = ~mask_indices
         else:
             x = features
             mask_indices = None
@@ -555,7 +560,7 @@ class HubertModel_6(BaseFairseqModel):
             layer=None if output_layer is None else output_layer - 1,
             tap=tap
         )
-
+        
         if features_only:
             return {"x": x, "padding_mask": padding_mask, "features": features}
         
@@ -568,7 +573,6 @@ class HubertModel_6(BaseFairseqModel):
         
         negs_1, _ = self.sample_negatives(y_1, y_1.size(1))
         negs_2, _ = self.sample_negatives(y_2, y_2.size(1))
-        
         z_1 = self.compute_sim(y_1, y_2, negs_1)
         z_2 = self.compute_sim(y_2, y_1, negs_2)
         z = torch.cat((z_1, z_2), dim=1)